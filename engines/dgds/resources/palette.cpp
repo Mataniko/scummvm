f270d447528b3f68091c64157799947d7f7663c5
Matan Bareket <mataniko@gmail.com>
2017-05-22 22:31:44 -0400

    Implement basic palette loading

--- engines/dgds/resources/palette.cpp
+++ engines/dgds/resources/palette.cpp
@@ -9,9 +9,26 @@
 
 namespace Dgds {
 
-Palette::Palette() {
+Palette::Palette(Resource *res) {
 	// TODO Auto-generated constructor stub
+	Resource *resVGA = res->getSubResource("PAL:")->getSubResource("VGA:");
 
+
+	_size = resVGA->size();
+	if (_size != 0x300) {
+		delete resVGA;
+		return;
+	}
+
+	for (int i = 0; i < 0x300; ++i) {
+		_pal[i] = resVGA->readByte();
+	}
+
+	delete resVGA;
+}
+
+byte *Palette::getPalette() {
+	return _pal;
 }
 
 Palette::~Palette() {
--- engines/dgds/resources/palette.h
+++ engines/dgds/resources/palette.h
@@ -1,12 +1,19 @@
 #ifndef DGDS_PALETTE_H_
 #define DGDS_PALETTE_H_
 
+#include "dgds/resource.h"
+
 namespace Dgds {
 
 class Palette {
 public:
-	Palette();
+	Palette(Resource *res);
+	byte *getPalette();
 	virtual ~Palette();
+
+private:
+	byte _pal[256 * 3];
+	uint32 _size;
 };
 
 } /* namespace Dgds */
